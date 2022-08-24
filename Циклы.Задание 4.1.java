5e46817a99755911240bd5483c7b25fbbaac355c
HDefender1 <111987074+HDefender1@users.noreply.github.com>
2022-08-24 17:07:34 +0300

    Add files via upload

---
+++ Циклы.Задание 4.1.java
@@ -0,0 +1,21 @@
+// Task #4.2
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 30;
+      int b = 50;
+      int sum = 0;
+      
+      if (a < b)
+      {
+      for(int i = a; i <= b; i++)
+      {
+        sum = sum + i;
+      
+       }
+      }
+      System.out.println("Sum of the range is " + sum);
+  }
+}
